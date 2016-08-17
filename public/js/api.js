56c99d1129d41deba683815baaf253a1cc4a0e9b
echentw <echentw@mit.edu>
2016-08-17 03:08:28 -0700

    fix race condition; refactor

---
+++ public/js/api.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+});
+
+var googleApiClientReady = function() {
+  gapi.client.setApiKey(API_KEY);
+  gapi.client.load('youtube', 'v3');
+  $('#search-button').attr('disabled', false);
+};
