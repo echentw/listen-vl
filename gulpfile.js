05702c53d209082215c05b168a7758f1dfa509da
echentw <echentw@mit.edu>
2017-01-16 16:29:02 -0500

    get ready for sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var server = require('gulp-develop-server');
 
 var serverJS = ["**/*.js", "!node_modules/**", '!bin/**'];
@@ -13,6 +14,25 @@ gulp.task('server:restart', function() {
   server.restart();
 });
 
-gulp.task('default', ['server:start'], function() {
+gulp.task('default', ['server:start', 'sass'], function() {
   gulp.watch(serverJS, ['server:restart']);
+  gulp.watch(paths.styles.files, ['sass']);
+});
+
+var paths = {
+  styles: {
+    src: './public/sass',
+    files: './public/sass/**/*.scss',
+    dest: './public/css'
+  }
+};
+
+gulp.task('sass', function() {
+  gulp.src(paths.styles.files)
+    .pipe(sass({
+      outputStyle: 'compressed',
+      sourceComments: 'map',
+      includePaths: [paths.styles.src]
+    }))
+    .pipe(gulp.dest(paths.styles.dest));
 });
