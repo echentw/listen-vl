c7ce349e6102bf8d9b2b427790c3bd3a4b2ca252
Eric Chen <echentw@mit.edu>
2017-01-21 23:11:46 -0500

    rename index.ejs -> search.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET search page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('search');
 });
 
 /* GET video page. */
