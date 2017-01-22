db31a5d8eb79697e8d4ed438268090a749d25bf4
Eric Chen <echentw@mit.edu>
2017-01-22 00:14:03 -0500

    ejs -> handlebars

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressHandlebars = require('express-handlebars');
 
 var routes = require('./routes/index');
 
@@ -11,7 +12,12 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('.hbs', expressHandlebars({
+  extname: '.hbs',
+  layoutsDir: 'views/layouts/',
+  partialsDir: 'views/partials/'
+}));
+app.set('view engine', '.hbs');
 
 app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
 app.use(logger('dev'));
